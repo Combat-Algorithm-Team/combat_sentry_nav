b2a30ed24c245bfbc0b58360bffaca2dd7f6e433
Muzjili <1679139025@qq.com>
2026-04-15 15:53:37 +0800

    change some config

--- pb2025_nav_bringup/launch/combat_navi_waypoint.launch.py
+++ pb2025_nav_bringup/launch/combat_navi_waypoint.launch.py
@@ -70,7 +70,7 @@ def generate_launch_description():
 
     declare_robot_name_cmd = DeclareLaunchArgument(
         "robot_name",
-        default_value="combat2025_sentry",
+        default_value="combat2026_sentry",
         description="The file name of the robot xmacro to be used",
     )
 
