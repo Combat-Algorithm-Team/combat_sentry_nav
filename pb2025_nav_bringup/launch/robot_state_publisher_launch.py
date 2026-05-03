ff01f6af537cd72fb2bcb6f61ec0e6d3c6870941
Muzjili <1679139025@qq.com>
2026-05-04 02:33:18 +0800

    add robot description launch file

--- pb2025_nav_bringup/launch/robot_state_publisher_launch.py
+++ pb2025_nav_bringup/launch/robot_state_publisher_launch.py
@@ -21,23 +21,26 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, GroupAction, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import PushRosNamespace, SetRemap
+from launch.actions import DeclareLaunchArgument, GroupAction, SetEnvironmentVariable
+from launch.conditions import IfCondition
+from launch.substitutions import Command, LaunchConfiguration, TextSubstitution
+from launch_ros.actions import Node, PushRosNamespace, SetRemap
 
 
 def generate_launch_description():
-    # Get the launch directory
-    pkg_pb2025_robot_description_dir = get_package_share_directory(
-        "pb2025_robot_description"
-    )
+    bringup_dir = get_package_share_directory("pb2025_nav_bringup")
 
     namespace = LaunchConfiguration("namespace")
     use_sim_time = LaunchConfiguration("use_sim_time")
     robot_name = LaunchConfiguration("robot_name")
+    robot_xmacro_file = LaunchConfiguration("robot_xmacro_file")
+    rviz_config_file = LaunchConfiguration("rviz_config_file")
+    use_rviz = LaunchConfiguration("use_rviz")
+    use_respawn = LaunchConfiguration("use_respawn")
+    log_level = LaunchConfiguration("log_level")
 
-    # Declare the launch arguments
+    robot_description = Command(["xacro ", robot_xmacro_file])
+
     declare_namespace_cmd = DeclareLaunchArgument(
         "namespace",
         default_value="",
@@ -46,48 +49,117 @@ def generate_launch_description():
 
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         "use_sim_time",
-        default_value="false",
+        default_value="False",
         description="Use simulation (Gazebo) clock if true",
     )
 
     declare_robot_name_cmd = DeclareLaunchArgument(
         "robot_name",
-        default_value="combat2026_sentry_robot",
+        default_value="combat2026_sentry",
         description="The file name of the robot xmacro to be used",
     )
+
+    declare_robot_xmacro_file_cmd = DeclareLaunchArgument(
+        "robot_xmacro_file",
+        default_value=[
+            TextSubstitution(text=os.path.join(bringup_dir, "xmacro", "")),
+            robot_name,
+            TextSubstitution(text=".urdf.xmacro"),
+        ],
+        description="The file path of the robot xmacro to be used",
+    )
+
+    declare_rviz_config_file_cmd = DeclareLaunchArgument(
+        "rviz_config_file",
+        default_value=os.path.join(bringup_dir, "rviz", "nav2_default_view.rviz"),
+        description="Full path to the RViz config file to use",
+    )
+
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+        "use_rviz",
+        default_value="False",
+        description="Whether to start RViz",
+    )
+
+    declare_use_respawn_cmd = DeclareLaunchArgument(
+        "use_respawn",
+        default_value="False",
+        description="Whether to respawn if a node crashes.",
+    )
+
+    declare_log_level_cmd = DeclareLaunchArgument(
+        "log_level",
+        default_value="info",
+        description="log level",
+    )
+
+    stdout_linebuf_envvar = SetEnvironmentVariable(
+        "RCUTILS_LOGGING_BUFFERED_STREAM", "1"
+    )
+
+    colorized_output_envvar = SetEnvironmentVariable("RCUTILS_COLORIZED_OUTPUT", "1")
 
     bringup_cmd_group = GroupAction(
         [
             PushRosNamespace(namespace=namespace),
             SetRemap("/tf", "tf"),
             SetRemap("/tf_static", "tf_static"),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    os.path.join(
-                        pkg_pb2025_robot_description_dir,
-                        "launch",
-                        "robot_description_launch.py",
-                    )
-                ),
-                launch_arguments={
-                    "namespace": namespace,
-                    "use_sim_time": use_sim_time,
-                    "robot_name": robot_name,
-                    "use_rviz": "False",
-                }.items(),
+            Node(
+                package="robot_state_publisher",
+                executable="robot_state_publisher",
+                output="screen",
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[
+                    {
+                        "use_sim_time": use_sim_time,
+                        "robot_description": robot_description,
+                        "publish_frequency": 200.0,
+                    },
+                ],
+                arguments=["--ros-args", "--log-level", log_level],
+            ),
+            Node(
+                package="joint_state_publisher",
+                executable="joint_state_publisher",
+                name="joint_state_publisher",
+                output="screen",
+                respawn=use_respawn,
+                respawn_delay=2.0,
+                parameters=[
+                    {
+                        "use_sim_time": use_sim_time,
+                        "rate": 200.0,
+                        "source_list": ["base_yaw_joint_publisher"],
+                        "offset_timestamp": 0.0,
+                    },
+                ],
+                arguments=["--ros-args", "--log-level", log_level],
+            ),
+            Node(
+                condition=IfCondition(use_rviz),
+                package="rviz2",
+                executable="rviz2",
+                arguments=["-d", rviz_config_file],
+                output="screen",
             ),
         ]
     )
 
-    # Create the launch description and populate
     ld = LaunchDescription()
 
-    # Declare the launch options
+    ld.add_action(stdout_linebuf_envvar)
+    ld.add_action(colorized_output_envvar)
+
     ld.add_action(declare_namespace_cmd)
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_robot_name_cmd)
+    ld.add_action(declare_robot_xmacro_file_cmd)
+    ld.add_action(declare_rviz_config_file_cmd)
+    ld.add_action(declare_use_rviz_cmd)
+    ld.add_action(declare_use_respawn_cmd)
+    ld.add_action(declare_log_level_cmd)
 
-    # Add the actions to launch all nodes
     ld.add_action(bringup_cmd_group)
 
     return ld
